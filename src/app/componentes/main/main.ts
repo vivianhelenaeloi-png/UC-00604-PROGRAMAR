b951eebbd0e5b2ffa12f9f29a6b1cb4a8d84c221
Vivian <vivianhelenaeloi@gmail.com>
2025-11-13 22:26:46 +0000

    Criar componentes

---
+++ src/app/componentes/main/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../node_modules/@angular/router/router_module.d";
+
+@Component({
+  selector: 'app-main',
+  imports: [RouterOutlet],
+  templateUrl: './main.html',
+  styleUrl: './main.css',
+})
+export class Main {
+
+}
