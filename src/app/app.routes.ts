726fd07a9a9779e37f8b5b9c4ee1307a55945013
Vivian <vivianhelenaeloi@gmail.com>
2025-11-27 21:12:59 +0000

    criar rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { Welcome } from './componentes/welcome/welcome';
 import { Routes } from '@angular/router';
+import { Uc00604 } from './pages/uc00604/uc00604';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //*Nossa primeira rota*//
+  {path:  '', redirectTo: "welcome", pathMatch:'full'},
+  {path: "welcome", loadComponent: () => import('./pages/welcome/welcome').then((vasco) => vasco.Welcome)},
+  {path:"pages/autenticacao",loadComponent: () => import('./pages/authentication/authentication').then(ze => ze.Authentication)},
+
+  //*Rota padrao sem lazyload*//
+  {path:"pages/uc00604",component: Uc00604},
+ 
+  //*Nossa ultima rota*//
+{path: "**", loadComponent: ()=> import('./pages/welcome/welcome').then(c => c.Welcome)},
+  {},
+];
---
+++ src/app/pages/uc00604/uc00604.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uc00604',
+  imports: [],
+  templateUrl: './uc00604.html',
+  styleUrl: './uc00604.css',
+})
+export class Uc00604 {
+
+}
